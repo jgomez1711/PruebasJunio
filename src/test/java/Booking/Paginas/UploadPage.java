d254c617f96a9e4f82a67e0b0b95729630111f4c
Johanna Patricia Gomez Beltran <johanitagomez17@gmail.com>
2021-06-26 12:09:04 -0500

    HU010 update

--- src/test/java/Booking/Paginas/UploadPage.java
+++ src/test/java/Booking/Paginas/UploadPage.java
@@ -1,5 +1,28 @@
 package Booking.Paginas;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import Booking.Steps.BotonesPaginas;
+import Booking.Steps.Questions;
+import net.serenitybdd.screenplay.Question;
+
 public class UploadPage {
 
+	private WebDriver driver;
+	private Questions questions = new Questions(driver);
+	private BotonesPaginas botonesPaginas = BotonesPaginas(driver);
+
+	public UploadPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+		this.driver = driver;
+
+	}
+
+
+		
+		
+		
+		
+	}
 }
